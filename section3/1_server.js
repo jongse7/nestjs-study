bdfff3e987ce3215249d9bca02434817ae8d1996
jongse7 <mmssjj72@naver.com>
2025-12-13 15:37:41 +0900

    section3-2

--- section3/1_server.js
+++ section3/1_server.js
@@ -1,6 +1,7 @@
 
 // import http from 'http'; - only typescript
 const http = require('http');
+const url = require('url');
 
 // localhost -> 127.0.0.1 -> loop back -> 서버를 실행한 컴퓨터
 const host = 'localhost';
@@ -9,6 +10,22 @@ const port = 3000;
 // req -> request -> 요청
 // res -> response -> 응답
 const server = http.createServer((req, res)=>{
+    const path = url.parse(req.url).pathname;
+
+    if(path === '/'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end('<h1>Home Page!</h1>');
+    }else if (path === '/post'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end('<h1>Post Page!</h1>');
+    }else if (path === '/user'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end('<h1>User Page!</h1>');
+    }else{
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end('<h1>404 Page Not Found!</h1>');
+    }
+
     // status code 200 / Header에 어떤 응답을 보내줄지 정의
     res.writeHead(200, {'Content-Type' : 'text/html'});
     
